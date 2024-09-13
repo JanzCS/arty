c089c6cf66135c0c30c7cc88413cbc2ff19c2d21
Jacob Janz <jacobjanzcs@gmail.com>
2024-09-12 21:59:10 -0500

    very messy commit just add everything so I can work on macbook

--- src/backend/routes/calculate.py
+++ src/backend/routes/calculate.py
@@ -1,25 +1,149 @@
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+import numpy as np
 import math
 
 # Define a router
 router = APIRouter()
 
+
 @router.post("/calculate")
-def calculate(mortar_easting: float, mortar_northing: float, observer_easting: float, observer_northing: float, obs_to_enemy_azimuth: float, obs_to_enemy_distance: float):
-    azimuth, distance = azimuth_from_observer_relative(mortar_easting, mortar_northing, observer_easting, observer_northing, obs_to_enemy_azimuth, obs_to_enemy_distance)
+def calculate(
+    mortar_easting: float,
+    mortar_northing: float,
+    observer_easting: float,
+    observer_northing: float,
+    obs_to_enemy_azimuth: float,
+    obs_to_enemy_distance: float,
+):
+    azimuth, distance = azimuth_from_observer_relative(
+        mortar_easting,
+        mortar_northing,
+        observer_easting,
+        observer_northing,
+        obs_to_enemy_azimuth,
+        obs_to_enemy_distance,
+    )
     return {"azimuth": round(azimuth, 2), "distance": round(distance, 2)}
+
 
 @router.post("/calculate2")
-def calculate2(mortar_easting: float, mortar_northing: float, enemy_easting: float, enemy_northing: float):
-    distance = distancefunc(mortar_easting, mortar_northing, enemy_easting, enemy_northing)
-    azimuth = azimuth_from_grids(mortar_easting, mortar_northing, enemy_easting, enemy_northing)
+def calculate2(
+    mortar_easting: float,
+    mortar_northing: float,
+    enemy_easting: float,
+    enemy_northing: float,
+):
+    distance = distancefunc(
+        mortar_easting, mortar_northing, enemy_easting, enemy_northing
+    )
+    azimuth = azimuth_from_grids(
+        mortar_easting, mortar_northing, enemy_easting, enemy_northing
+    )
     return {"azimuth": round(azimuth, 2), "distance": round(distance, 2)}
 
-def azimuth_from_observer_relative(mortar_easting: float, mortar_northing: float, observer_easting: float, observer_northing: float, obs_to_enemy_azimuth: float, obs_to_enemy_distance: float):
-    # First need to calculate easting and northing of enemy relative to observer
-    obs_to_enemy_easting = 0.
-    obs_to_enemy_northing = 0.
+
+# upper_half determine if the user wants to raise or low the elevation from
+# 45 degrees to reach target (mortar vs howitzer)
+@router.post("/calculate3")
+def calculate_elevation(initial_velocity: float, target_height: float, distance: float, upper_half: bool):
+    current_elevation = 45.
+    # Check if target is even reachable using optimal launch angle
+    current_distance = ballistic_sim3(
+        initial_velocity, current_elevation, target_height, 0.001
+    )[0]
+    if current_distance < distance:
+        # target unreachable
+        return HTMLResponse("Target Unreachable.", status_code=400)
+    while current_distance - distance > 0.1:
+        if upper_half:
+            current_elevation = current_elevation + 0.01
+        else:
+            current_elevation = current_elevation - 0.01
+        current_distance = ballistic_sim3(
+            initial_velocity, current_elevation, target_height, 0.001
+        )[0]
+    return {
+        "elevation": round(math.radians(current_elevation) * 1000, 0),
+        "time_to_impact": round(ballistic_sim3(
+            initial_velocity, current_elevation, target_height, 0.001
+        )[1], 1),
+    }
+
+
+def ballistic_sim(initial_velocity, elevation, target_height, delta_t):
+    t = 0
+    x = 0
+    y = 0
+    v_0 = math.sin(math.radians(elevation)) * initial_velocity
+    v_y = math.sin(math.radians(elevation)) * initial_velocity
+    v_x = math.cos(math.radians(elevation)) * initial_velocity
+    while v_y > 0 or y > target_height:
+        t = t + delta_t
+        x = x + v_x * delta_t
+        y = y + v_y * delta_t
+        v_y = v_0 - 9.81 * t
+    return x, t
+
+
+def ballistic_sim2(initial_velocity, elevation, target_height, delta_t):
+    f = .00031  # from arma for 81mm
+    t = 0
+    x = 0
+    y = 0
+    v_y = math.sin(math.radians(elevation)) * initial_velocity
+    v_x = math.cos(math.radians(elevation)) * initial_velocity
+    while (v_y > 0 or y > target_height):
+        v = math.sqrt(v_x ** 2 + v_y ** 2)
+        deceleration = f * v ** 2
+        v_theta = math.atan(v_y / v_x)
+        # decel_x = -1 * math.cos(v_theta) * deceleration
+        # decel_y = -1 * math.sin(v_theta) * deceleration
+        decel_x = -1 * deceleration
+        decel_y = -1 * deceleration
+        decel_y = decel_y - 9.81
+        
+        t = t + delta_t
+        x = x + v_x * delta_t
+        y = y + v_y * delta_t
+        v_x = v_x + (decel_x * delta_t)
+        v_y = v_y + (decel_y * delta_t)
+    return x, t
+
+
+def ballistic_sim3(initial_velocity, elevation, target_height, delta_t):
+    f = -.00031  # from arma for 81mm
+    t = 0
+    position = np.array([0, 0])
+    gravity = np.array([0, -9.81])
+    velocity = initial_velocity * np.array([math.cos(math.radians(elevation)), math.sin(math.radians(elevation))])
+    while (velocity[1] > 0 or position[1] > target_height):
+        position = position + velocity * delta_t
+
+        speed = np.linalg.norm(velocity)
+        acceleration = (velocity * speed * f) + gravity
+
+        velocity = velocity + acceleration * delta_t
+        t = t + delta_t
+    return position[0], t
+
+
+# FIXME: Not taking into account relative height from observer, which means I'm
+# calculating eastings/northings based on hypotenuse of the triangle rather
+# than the actual x/y distance
+def azimuth_from_observer_relative(
+    mortar_easting: float,
+    mortar_northing: float,
+    observer_easting: float,
+    observer_northing: float,
+    obs_to_enemy_azimuth: float,
+    obs_to_enemy_distance: float,
+):
+    # First need to calculate easting and northing of enemy
+    # relative to observer
+    obs_to_enemy_easting = 0.0
+    obs_to_enemy_northing = 0.0
 
     # Due North
     if obs_to_enemy_azimuth == 0:
@@ -38,17 +162,23 @@ def azimuth_from_observer_relative(mortar_easting: float, mortar_northing: float
     elif obs_to_enemy_azimuth < 180:
         angle = obs_to_enemy_azimuth - 90
         obs_to_enemy_easting = math.cos(math.radians(angle)) * obs_to_enemy_distance
-        obs_to_enemy_northing = -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_northing = (
+            -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        )
     # Due South
     elif obs_to_enemy_azimuth == 180:
         obs_to_enemy_easting = 0
         obs_to_enemy_northing = -1 * obs_to_enemy_distance
-        
+
     # Third Quadrant
     elif obs_to_enemy_azimuth < 270:
         angle = 270 - obs_to_enemy_azimuth
-        obs_to_enemy_easting = -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
-        obs_to_enemy_northing = -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_easting = (
+            -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        )
+        obs_to_enemy_northing = (
+            -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        )
     # Due West
     elif obs_to_enemy_azimuth == 270:
         obs_to_enemy_easting = -1 * obs_to_enemy_distance
@@ -56,19 +186,29 @@ def azimuth_from_observer_relative(mortar_easting: float, mortar_northing: float
     # Fourth Quadrant
     else:
         angle = obs_to_enemy_azimuth - 270
-        obs_to_enemy_easting = -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_easting = (
+            -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        )
         obs_to_enemy_northing = math.sin(math.radians(angle)) * obs_to_enemy_distance
 
     enemy_easting = observer_easting + obs_to_enemy_easting
     enemy_northing = observer_northing + obs_to_enemy_northing
 
-    distance = distancefunc(mortar_easting, mortar_northing, enemy_easting, enemy_northing)
+    distance = distancefunc(
+        mortar_easting, mortar_northing, enemy_easting, enemy_northing
+    )
 
-    return azimuth_from_grids(mortar_easting, mortar_northing, enemy_easting, enemy_northing), distance
+    return (
+        azimuth_from_grids(
+            mortar_easting, mortar_northing, enemy_easting, enemy_northing
+        ),
+        distance,
+    )
 
 
 def distancefunc(x1, y1, x2, y2):
     return math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
+
 
 def azimuth_from_grids(start_easting, start_northing, end_easting, end_northing):
     east_vector = end_easting - start_easting
@@ -80,16 +220,24 @@ def azimuth_from_grids(start_easting, start_northing, end_easting, end_northing)
     temp = north_vector
     north_vector = east_vector
     east_vector = temp
-    
 
     initial = math.degrees(math.atan2(north_vector, east_vector))
     if initial < 0:
         initial = 360 + initial
-    
+
     return initial
 
-def enemy_pos_from_observer_relative(mortar_easting, mortar_northing, observer_easting, observer_northing, obs_to_enemy_azimuth, obs_to_enemy_distance):
-    # First need to calculate easting and northing of enemy relative to observer
+
+def enemy_pos_from_observer_relative(
+    mortar_easting,
+    mortar_northing,
+    observer_easting,
+    observer_northing,
+    obs_to_enemy_azimuth,
+    obs_to_enemy_distance,
+):
+    # First need to calculate easting and northing of enemy
+    # relative to observer
     obs_to_enemy_easting = 0
     obs_to_enemy_northing = 0
 
@@ -110,17 +258,23 @@ def enemy_pos_from_observer_relative(mortar_easting, mortar_northing, observer_e
     elif obs_to_enemy_azimuth < 180:
         angle = obs_to_enemy_azimuth - 90
         obs_to_enemy_easting = math.cos(math.radians(angle)) * obs_to_enemy_distance
-        obs_to_enemy_northing = -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_northing = (
+            -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        )
     # Due South
     elif obs_to_enemy_azimuth == 180:
         obs_to_enemy_easting = 0
         obs_to_enemy_northing = -1 * obs_to_enemy_distance
-        
+
     # Third Quadrant
     elif obs_to_enemy_azimuth < 270:
         angle = 270 - obs_to_enemy_azimuth
-        obs_to_enemy_easting = -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
-        obs_to_enemy_northing = -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_easting = (
+            -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        )
+        obs_to_enemy_northing = (
+            -1 * math.sin(math.radians(angle)) * obs_to_enemy_distance
+        )
     # Due West
     elif obs_to_enemy_azimuth == 270:
         obs_to_enemy_easting = -1 * obs_to_enemy_distance
@@ -128,7 +282,9 @@ def enemy_pos_from_observer_relative(mortar_easting, mortar_northing, observer_e
     # Fourth Quadrant
     else:
         angle = obs_to_enemy_azimuth - 270
-        obs_to_enemy_easting = -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        obs_to_enemy_easting = (
+            -1 * math.cos(math.radians(angle)) * obs_to_enemy_distance
+        )
         obs_to_enemy_northing = math.sin(math.radians(angle)) * obs_to_enemy_distance
 
     enemy_easting = observer_easting + obs_to_enemy_easting
--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+// current 81mm mortar estimated v_0: 121.305
+
 function App() {
   const [mortar_easting, setmortar_easting] = useState(0);
   const [mortar_northing, setmortar_northing] = useState(0);
@@ -9,13 +11,15 @@ function App() {
   const [enemy_northing, setenemy_northing] = useState(0);
   const [obs_to_enemy_azimuth, setobs_to_enemy_azimuth] = useState(0);
   const [obs_to_enemy_distance, setobs_to_enemy_distance] = useState(0);
+  const [target_height, settarget_height] = useState(0);
   const [obsResult, setobsResult] = useState(null);
   const [noObsResult, setnoObsResult] = useState(null);
+  const [obsElevation, setObsElevation] = useState(null);
 
   const submitObserver = async (e) => {
     e.preventDefault();
 
-    const queryParams = new URLSearchParams({
+    const azimuth_queryParams = new URLSearchParams({
       mortar_easting: mortar_easting,
       mortar_northing: mortar_northing,
       observer_easting: observer_easting,
@@ -24,12 +28,26 @@ function App() {
       obs_to_enemy_distance: obs_to_enemy_distance
     }).toString();
 
-    const response = await fetch(`http://127.0.0.1:8000/calculate?${queryParams}`, {
+    const azimuth_response = await fetch(`http://127.0.0.1:8000/calculate?${azimuth_queryParams}`, {
       method: 'POST'
     });
 
-    const data = await response.json();
-    setobsResult(data);
+    const azimuth_data = await azimuth_response.json();
+    setobsResult(azimuth_data);
+
+    const elevation_queryParams = new URLSearchParams({
+      initial_velocity: 375,
+      target_height: target_height,
+      distance: azimuth_data.distance,
+      upper_half: true
+    }).toString();
+
+    const elevation_response = await fetch(`http://127.0.0.1:8000/calculate3?${elevation_queryParams}`, {
+      method: 'POST'
+    });
+
+    const elevation_data = await elevation_response.json();
+    setObsElevation(elevation_data)
   };
 
   const submitNoObserver = async (e) => {
@@ -47,7 +65,7 @@ function App() {
     });
 
     const data = await response.json();
-    setnoObsResult(data);
+    setobsResult(data);
   };
 
   return (
@@ -169,14 +187,25 @@ function App() {
             />
           </label>
           <br />
+          <label>
+            Target height:
+            <input
+              type="number"
+              value={target_height}
+              onChange={(e) => settarget_height(parseFloat(e.target.value))}
+              required
+            />
+          </label>
+          <br />
           <button type="submit">Calculate</button>
         </form>
 
         {/* Display the result inside the box below the button */}
-        {obsResult !== null && (
+        {obsResult !== null && obsElevation !== null && (
           <div className="result">
             <h2>Azimuth: {obsResult.azimuth}</h2>
-            <h2>Distance: {obsResult.distance}</h2>
+            <h2>Elevation: {obsElevation.elevation}</h2>
+            <h2>Time to Impact: {obsElevation.time_to_impact}</h2>
           </div>
         )}
       </div>
